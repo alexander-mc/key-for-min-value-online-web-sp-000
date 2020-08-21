847afb468db9f7cc91df1cd92e1caec70d61dc17
Alexander Clamor <alexander.clamor@gmail.com>
2020-08-21 19:21:04 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,10 @@ def key_for_min_value(name_hash)
   min_value = 0
   min_key = ""
   name_hash.each do |key, value|
-    min_value = value, min_key = key if value < min_value
+    if value < min_value
+      min_value = value
+      min_key = key 
+    end
   end
   min_key
 end
