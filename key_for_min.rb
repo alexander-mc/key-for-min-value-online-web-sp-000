efe6a4697c743f263f182a9297fd1f4df4578293
Alexander Clamor <alexander.clamor@gmail.com>
2020-08-21 19:24:29 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -10,6 +10,6 @@ def key_for_min_value(name_hash)
   min_key
 end
 
-veggies = {"apple" => -45, "banana" => -44.5, "carrot" => -44.9}
+veggies = {:apple => -45, :banana => -44.5, :carrot => -44.9}
 
 puts key_for_min_value(veggies)
